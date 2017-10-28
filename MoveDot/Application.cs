7f0ae7676ffbe0643c7564f7ff0c2fe974fad675
bhq12 <bhq12@uclive.ac.nz>
2017-10-28 14:50:27 +1300

    use new Controller in app

--- MoveDot/Application.cs
+++ MoveDot/Application.cs
@@ -1,7 +1,30 @@
-﻿namespace MoveDot
+﻿using System.ComponentModel;
+using MoveDot.Controller;
+using MoveDot.Model;
+using MoveDot.View;
+
+namespace MoveDot
 {
     public class Application
     {
+        private Dot _dot;
+        private IDotView _view;
+        private IDotController _controller;
+
+        public Application()
+        {
+            _dot = new Dot();
+            //_view = new SimpleDotView(_dot);
+            _view = new ConsoleDotView(_dot);
+            _controller = new KeyboardDotController(_dot, _view);
+            
+        }
+
+        public void StartUp()
+        {
+            _view.Update();
+            _controller.Run();
+        }
         
     }
 }
