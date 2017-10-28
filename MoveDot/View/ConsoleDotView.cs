e3f9c5a9f5258626a315f6ce859c90bedb5166e2
bhq12 <bhq12@uclive.ac.nz>
2017-10-28 14:49:25 +1300

    implement console controller, add new bugs

--- MoveDot/View/ConsoleDotView.cs
+++ MoveDot/View/ConsoleDotView.cs
@@ -1,7 +1,36 @@
-﻿namespace MoveDot.View
+﻿using MoveDot.Model;
+using System;
+using System.Text;
+
+namespace MoveDot.View
 {
-    public class ConsoleDotView
+    public class ConsoleDotView : IDotView
     {
+        private Dot _dot;
         
+        public ConsoleDotView(Dot dot)
+        {
+            _dot = dot;
+        }
+
+        public void Update()
+        {
+            var position = _dot.Position;
+            var width = Console.WindowWidth;
+            var height = Console.WindowHeight;
+
+            for (int i = 0; i < height; i++)
+            {
+                if (i == position.Y % height)
+                {
+                    var line = new String(' ', position.X - 1) + "." + new String(' ', width - position.X);
+                    Console.WriteLine(line);
+                }
+                else
+                {
+                    Console.WriteLine(new String(' ', width));
+                }
+            }
+        }
     }
 }
