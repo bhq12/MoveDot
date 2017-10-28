d1777c3deaf6734c043b5f804cc7caa688e3e8a8
bhq12 <bhq12@uclive.ac.nz>
2017-10-28 13:46:06 +1300

    add simple view

---
+++ MoveDot/View/SimpleView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using System.Xml.Xsl.Runtime;
+using MoveDot.Model;
+
+namespace MoveDot.View
+{
+    public class SimpleView
+    {
+        private Dot _dot;
+        public SimpleView(Dot dot)
+        {
+            _dot = dot;
+        }
+
+        public void Update()
+        {
+            var position = _dot.Position;
+            Console.WriteLine("Dot: (" + position.X + "," + position.Y + ")");
+        }
+    }
+}
